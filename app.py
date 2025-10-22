80aad6721d70c02909c44576393237666bc2d442
gabe <gabe@gabes-MacBook-Pro.local>
2025-10-22 12:59:48 -0400

    Initial commit: Concrete beam design calculator with ACI compliance

---
+++ app.py
@@ -0,0 +1,76 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "c3ccd486-8741-4cee-9f6e-1362c9209abd",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "\n",
+      "--- Reinforced Concrete As Solver ---\n",
+      "Units: fc & fy in ksi, b & d in inches, Mn in kip-ft\n",
+      "\n",
+      "\n",
+      "--- Results ---\n",
+      "--- Inputs Used ---\n",
+      "  f'c:   5.0 ksi\n",
+      "  fy:    60.0 ksi\n",
+      "  b:     36.0 in\n",
+      "  d:     78.0 in\n",
+      "  phi:   0.9\n",
+      "  Mn:    19600.0 kip-ft\n",
+      "-------------------\n",
+      "\n",
+      "Possible steel areas As (in^2):\n",
+      "  330.611\n",
+      "  67.189\n",
+      "\n",
+      "⚠️  Use the smaller positive As for under-reinforced design.\n"
+     ]
+    }
+   ],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "ddfb9dee-af92-47aa-8648-39a8bf678bd0",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "b3b6de61-7d2a-4684-bf8c-ae53b2d319f9",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.9.6"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
